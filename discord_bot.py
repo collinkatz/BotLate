4080b059dafcfc4567e0c1b70a144262ee949fb6
Abhinav Modugula <abhimodugula@gmail.com>
2021-04-30 00:03:36 -0400

    added instr for quiz

--- discord_bot.py
+++ discord_bot.py
@@ -13,13 +13,16 @@ async def on_ready():
 def list_quizes():
     """Returns a string of available quizzes"""
     s = ""
-    s += "Usage: -bl quiz 1 OR -bl quiz occupations\n"
+    s += "Usage: -bl quiz occupations\n"
     s += "1) Occupations\n"
     # Write this with real code
     return s
 
-
-
+def quiz_dict():
+    """A dict that maps a quiz name to its id"""
+    d = {}
+    d["occupations"] = 1
+    return d
 
 @client.event
 async def on_message(message):
@@ -45,6 +48,17 @@ async def on_message(message):
             voice_channel = await channel.connect()
         except discord.errors.ClientException:
             await message.channel.send(list_quizes())
+
+    if message.content.startswith('-bl quiz'):
+        """
+        Allows the user to take a quiz. Needs to get the quiz name,
+        then the id from a dict. Then, create a Content object, and then get
+        the specific Quiz. Then, the bot should speak verbally the question
+        and allow the user the type to answer. The Quiz object will tell if its
+        correct and then the bot can relay that info. If the quiz is done, the score
+        shoud be displayed.
+        """
+        pass
 
     if message.content.startswith('-bl leave'):
         voice_client = client.voice_clients[0]
